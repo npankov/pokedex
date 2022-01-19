d6bf24b8832ca3b9c690032ae009e0bffc227dd6
Nikolai Pankov <pankoff2000@gmail.com>
2022-01-19 15:50:09 +0100

    list pokemon added

--- pokedex/src/app/components/pokemon/pokemon.component.ts
+++ pokedex/src/app/components/pokemon/pokemon.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Pokemon } from '../../models';
 
 @Component({
@@ -7,18 +7,10 @@ import { Pokemon } from '../../models';
   styleUrls: ['./pokemon.component.css']
 })
 export class PokemonComponent implements OnInit {
-  Bulbasaur: Pokemon = {
-    nom: 'Bulbasaur',
-    numero: '1',
-    type: 'Seed',
-    description: ' There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger. ',
-    taille: '2\' 04',
-    poids: '15.2 lbs',
-    evolution: 'no evolu',
-    image: './assets/image/Bulbasaur.png'
-  };
+  @Input() pokemon!: Pokemon;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
