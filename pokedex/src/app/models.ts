8403359df87b0779279f6b19b7b0ef0677665230
jeremie malezieux <jeremie.malezieux@gmail.com>
2022-01-19 14:07:02 +0100

    modification

---
+++ pokedex/src/app/models.ts
@@ -0,0 +1,15 @@
+export type Pokemon = {
+    nom: string,
+    numero: string,
+    type: string,
+    description: string,
+    taille: string,
+    poids: string,
+    evolution: string,
+
+
+}
+
+
+
+
