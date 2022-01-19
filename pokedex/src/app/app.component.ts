b1c8a93555c74f4e614d15928726e62da11c4f11
jeremie malezieux <jeremie.malezieux@gmail.com>
2022-01-19 14:10:18 +0100

    index

--- pokedex/src/app/app.component.ts
+++ pokedex/src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pokedex';
+  nom = "Salamèche";
+  numero = "#004";
+  type = "Feu";
+  description = "Il préfère les endroits chauds. En cas de pluie, de la vapeur
+    se forme autour de sa queue."
+  taille = "0.6 m"
+  poids = "8.5 kg"
+  evolution = "Reptincel"
 }
