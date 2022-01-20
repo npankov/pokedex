82d57f505e36eff0d4d15a07f23c410d3fae267a
npankov <pankoff2000@gmail.com>
2022-01-20 12:08:39 +0100

    update project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { DetaillePageComponent } from './pages/detaille-page/detaille-page.component';
 import { PrincipalPageComponent } from './pages/principal-page/principal-page.component';
 import { PokemonComponent } from './components/pokemon/pokemon.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,13 @@ import { PokemonComponent } from './components/pokemon/pokemon.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
